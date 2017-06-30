ecfedf9370e0b4a07722963fbbd77ff8a492cdbc
NeonWizard <averagewizard13@gmail.com>
2017-06-29 20:56:51 -0400

    Fully functional autonomous script

--- main.py
+++ main.py
@@ -13,5 +13,38 @@ import time
 from twython import Twython, TwythonError, TwythonRateLimitError
 from boto.s3.connection import S3Connection
 
-twitter_api = Twython(os.environ["TWITTER_API_KEY"], os.environ["TWITTER_API_SECRET"], os.environ["TWITTER_TOKEN"], os.environ["TWITTER_TOKEN_SECRET"])
-s3_api = S3Connection(os.environ["S3_KEY"], os.environ["S3_SECRET"])
+twitterAPI = Twython(os.environ["TWITTER_API_KEY"], os.environ["TWITTER_API_SECRET"], os.environ["TWITTER_TOKEN"], os.environ["TWITTER_TOKEN_SECRET"])
+s3API = S3Connection(os.environ["S3_KEY"], os.environ["S3_SECRET_KEY"])
+
+bucket = s3API.get_bucket("wespooky-twittermanagement")
+key = bucket.get_key("followers.txt")
+
+# ======================
+#   -=- CODE TIME -=-
+# ======================
+
+# == Load Sets ==
+lastrunFollowers = set([int(follower) for follower in key.get_contents_as_string().decode().split("|")])
+currentFollowers = set(twitterAPI.get_followers_ids()["ids"])
+
+# == Follow Back ==
+# DESC: Follow everyone in current follower list who isn't in lastrun follower list
+toFollow = currentFollowers.difference(lastrunFollowers)
+
+for ID in toFollow:
+	twitterAPI.create_friendship(user_id=ID)
+	print("Followed {}.".format(ID))
+
+# == Unfollow Unfollowers ==
+# DESC: Unfollow everyone in lastrun follower list who isn't in current follower list.
+toUnfollow = lastrunFollowers - currentFollowers
+
+for ID in toUnfollow:
+	twitterAPI.destroy_friendship(user_id=ID)
+	print("Unfollowed {}.".format(ID))
+
+
+# == Save Set ==
+key.set_contents_from_string(
+	"|".join([str(follower) for follower in currentFollowers])
+)
